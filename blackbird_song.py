4bd9f6b1f28e0ae02e180f469a0acdee2350a2bf
ksenek <110329390+ksenek@users.noreply.github.com>
2025-03-26 08:26:11 +1300

    Getting data into a module using the song blackbird
    A module which takes enough inputs to receive the text for each of the differing sections. In the module, the code prints out the verse (including the input data). This module is called twice, once for each verse, sending the correct information to print the verses.

---
+++ blackbird_song.py
@@ -0,0 +1,10 @@
+#print lyrics for 2 verses using parameters
+
+def verse(takeThese,learnTo,momentTo):
+    print("Blackbird singing in the dead of night"
+             "Take these ", takeThese, " and learn to ", learnTo,
+             "All your life"
+             "You were only waiting for this moment to ",momentTo)
+
+verse("broken wings", "fly", "arise")
+verse("sunken eyes", "see", "be free")
