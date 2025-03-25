502e5dbdb1878d12be0c8c2d62b57e6475788f1f
ksenek <110329390+ksenek@users.noreply.github.com>
2025-03-26 08:42:18 +1300

    Mystery Calculator
    Ask someone to choose a number from any of the six cards. Show them each card in turn and ask them if their number appears on it. Print out the list of numbers in the card. Add the numbers in the top left-hand corner of each card that contains their number. The total is their number. Output the user's number

---
+++ mystery_calc_template.py
@@ -0,0 +1,58 @@
+##
+# mystery_calc.py
+# You should separate populating the card and printing out the card
+# But for simplicity they are combined here.
+
+
+def card_1():
+    """
+    Populate and print card 1
+    """
+    pass
+
+
+def card_2():
+    """
+    Populate and print card 2
+    """
+    pass
+
+
+def card_3():
+    """
+    Populate and print card 3
+    """
+    pass
+
+
+def card_4():
+    """
+    Populate and print card 4
+    """
+    pass
+
+
+def card_5():
+    """
+    Populate and print card 5
+    """
+    pass
+
+
+def card_6():
+    """
+    Populate and print card 6
+    """
+    pass
+
+
+
+# Main routine
+if __name__ == "__main__":
+    # Print each card
+
+
+    # If the user answers their number is in the card add to total
+
+
+    # Print out total
