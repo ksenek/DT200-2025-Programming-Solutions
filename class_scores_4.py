626610f2b312983c1e8e4a31f44bcedc4f7f9af0
ksenek <110329390+ksenek@users.noreply.github.com>
2025-03-26 08:37:04 +1300

    Class Scores 4
    Added functionality to class scores 3 that will allow the user to modify a score in either of the two classes

---
+++ class_scores_4.py
@@ -0,0 +1,86 @@
+##
+#class_scores_4.py
+
+#List initial scores for both classes
+class_a_scores = [44, 33, 28, 47, 12, 28, 32, 31, 11, 39, 40, 26, 36]
+class_b_scores = [19, 26, 38, 31, 30, 42, 44, 14, 27, 43, 46, 49, 24, 26, 36]
+
+def calculate_scores(scores):
+    """Function to calculate the minimum, maximum and mean scores for a given list of scores"""
+    minimum_score = scores[0]
+    maximum_score = scores[0]
+    total_score = 0
+    for score in scores:
+        if score < minimum_score:
+            minimum_score = score
+        if score > maximum_score:
+            maximum_score = score
+        total_score += score
+    mean_score = total_score / len(scores)
+    return minimum_score, maximum_score, mean_score
+
+
+def modify_score(class_choice, scores):
+    """Function to modify a score in either of the two classes"""
+    index = int(input(f"Enter the index of the score you want to modify in Class {class_choice}: "))
+    if index < 0 or index >= len(scores):
+        print("Index out of range. No modification done.")
+        return
+    new_score = int(input("Enter the new score: "))
+    scores[index] = new_score
+
+
+# Calculate the minimum, maximum and mean scores for both classes and print them out
+min_a, max_a, mean_a = calculate_scores(class_a_scores)
+min_b, max_b, mean_b = calculate_scores(class_b_scores)
+min_both, max_both, mean_both = calculate_scores(class_a_scores + class_b_scores)
+print("Class A minimum score:", min_a)
+print("Class A maximum score:", max_a)
+print("Class A mean score:", mean_a)
+print("Class B minimum score:", min_b)
+print("Class B maximum score:", max_b)
+print("Class B mean score:", mean_b)
+print("Both classes minimum score:", min_both)
+print("Both classes maximum score:", max_both)
+print("Both classes mean score:", mean_both)
+
+# Main loop to allow the user to add or modify scores
+if __name__ == '__main__':
+    while True:
+        action = input("Enter A to add a score, M to modify a score, or Q to quit: ")
+        if action.upper() == "Q":
+            False
+        elif action.upper() == "A":
+            class_choice = input("Enter A or B to add a score to that class: ")
+            if class_choice.upper() == "A":
+                score = int(input("Enter the score to add to Class A: "))
+                class_a_scores.append(score)
+            elif class_choice.upper() == "B":
+                score = int(input("Enter the score to add to Class B: "))
+                class_b_scores.append(score)
+            else:
+                print("Invalid input, please try again.")
+        elif action.upper() == "M":
+            class_choice = input("Enter A or B to modify a score in that class: ")
+            if class_choice.upper() == "A":
+                modify_score("A", class_a_scores)
+            elif class_choice.upper() == "B":
+                modify_score("B", class_b_scores)
+            else:
+                print("Invalid input, please try again.")
+        else:
+            print("Invalid input, please try again.")
+
+    # Recalculate the minimum, maximum and mean scores for both classes and print them out again
+    min_a, max_a, mean_a = calculate_scores(class_a_scores)
+    min_b, max_b, mean_b = calculate_scores(class_b_scores)
+    min_both, max_both, mean_both = calculate_scores(class_a_scores + class_b_scores)
+    print("Updated Class A minimum score:", min_a)
+    print("Updated Class A maximum score:", max_a)
+    print("Updated Class A mean score:", mean_a)
+    print("Updated Class B minimum score:", min_b)
+    print("Updated Class B maximum score:", max_b)
+    print("Updated Class B mean score:", mean_b)
+    print("Updated both classes minimum score:", min_both)
+    print("Updated both classes maximum score:", max_both)
+    print("Updated both classes mean score:", mean_both)
